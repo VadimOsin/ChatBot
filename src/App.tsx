6a91086a8335ebc6a1047c85e49dca7e5b5cb665
VadimOsin <vadzim.osin2001@gmail.com>
2023-08-16 09:57:08 +0300

    init project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Chat from './components/Chat';
+import './App.css'
+const App: React.FC = () => {
+    return (
+        <div className="app">
+            <Chat />
+        </div>
+    );
+};
+
+export default App;
